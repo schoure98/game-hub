5bdc0410d2213cd42948af7c738accfbfc7a134d
Shreya Surendra Choure <schoure@pdx.edu>
2023-10-26 17:13:19 -0700

    mobile responsiveness added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Button } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
-  )
-}
+	return (
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`,
+			}}
+		>
+			<GridItem area={"nav"} bg="orange.300">
+				Nav
+			</GridItem>
+			<Show above="lg">
+				<GridItem area={"aside"} bg="green.300">
+					Aside
+				</GridItem>
+			</Show>
+			<GridItem area={"main"} bg="blue.300">
+				Main
+			</GridItem>
+		</Grid>
+	);
+};
 
-export default App
+export default App;
